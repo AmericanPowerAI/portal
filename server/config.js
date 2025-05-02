243807094a6ddf308a183c41b682d0d4dbbb7f5b
AmericanPowerAI <lakefrontentertainment5@gmail.com>
2025-05-01 20:09:52 -0400

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,15 +1,48 @@
 module.exports = {
-  // Remove OpenAI config
-  SEARCH_QUERIES: [
-    "companies outsourcing call center work",
-    "IT staffing agencies with overflow",
-    "companies hiring remote web designers",
-    "businesses needing photo editing services",
-    "companies with hiring freezes but growing demand"
-  ],
-  // Add local model paths
-  MODEL_PATHS: {
-    needsClassifier: './server/ai/models/needs-classifier',
-    approachGenerator: './server/ai/models/approach-generator'
+  JOB_SITES: {
+    GENERAL: [
+      'https://www.indeed.com',
+      'https://www.linkedin.com/jobs',
+      'https://www.glassdoor.com/Job',
+      'https://www.monster.com',
+      'https://www.careerbuilder.com',
+      'https://www.ziprecruiter.com'
+    ],
+    CALL_CENTER: [
+      'https://www.callcenterjobs.com',
+      'https://www.remote.co/remote-call-center-jobs',
+      'https://www.flexjobs.com/search?search=call+center',
+      'https://www.specialtyjobmarkets.com/call_center_jobs.html'
+    ],
+    IT_TECH: [
+      'https://www.dice.com',
+      'https://www.techcareers.com',
+      'https://www.crunchboard.com',
+      'https://www.authenticjobs.com',
+      'https://www.stackoverflow.com/jobs',
+      'https://www.angel.co/jobs'
+    ],
+    FREELANCE: [
+      'https://www.upwork.com',
+      'https://www.fiverr.com',
+      'https://www.toptal.com',
+      'https://www.freelancer.com'
+    ]
+  },
+  SEARCH_QUERIES: {
+    CALL_CENTER: [
+      "call center outsourcing",
+      "customer service overflow",
+      "remote call agents needed",
+      "third party call center",
+      "BPO services needed"
+    ],
+    IT_TECH: [
+      "IT staff augmentation",
+      "tech outsourcing",
+      "managed IT services",
+      "offshore development team",
+      "remote developers needed"
+    ]
   }
 };
